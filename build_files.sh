a733fb408a1e2f967f7d112b70a09c7e62c1ab25
its.S4lem <salem4awata@gmail.com>
2026-09-18 17:48:04 +0200

    fix: use virtualenv in build script to bypass PEP 668

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
 echo "BUILD END"
